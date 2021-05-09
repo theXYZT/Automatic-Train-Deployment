f5570fae9c7a6dfacf6b59fcfcb61261baa038b5
Nikhil Mahajan <nikhilm92@gmail.com>
2021-05-09 19:36:13 -0400

    updated

--- control.lua
+++ control.lua
@@ -51,7 +51,7 @@ script.on_event(defines.events.on_train_changed_state, function (event)
 
 	--is a train heading to a copy/dandom stop?
 	elseif trigger.state == defines.train_state.on_the_path and trigger.path_end_stop and trigger.path_end_stop.valid and
-		(trigger.path_end_stop.name == AutomaticTrainDeployment_defines.names.entities.copyStop or 
+		(trigger.path_end_stop.name == AutomaticTrainDeployment_defines.names.entities.copyStop or
 		trigger.path_end_stop.name == AutomaticTrainDeployment_defines.names.entities.randomStop) then
 
 		local trigger_stop = trigger.path_end_stop
@@ -95,22 +95,13 @@ script.on_event(defines.events.on_train_changed_state, function (event)
 
 			if circuitSpeed then
 				speed = (((green and green.get_signal{ type = "virtual", name = "signal-S"}) or 0) + ((red and red.get_signal{ type = "virtual", name = "signal-S"}) or 0))
+				speed = speed / 216
 			end
 		end
 
 		--set the train to automatic
-		new.manual_mode = not auto 
+		new.manual_mode = not auto
 		new.speed = (circuitSpeed and speed > 0 and speed) or 0
+		game.players[1].print("Train created with speed: " .. new.speed)
 	end
 end)
-
---varoius console commands I used to debug things
---/c game.player.selected.connect_rolling_stock(defines.rail_direction.front)
---/c game.player.print(game.player.selected.train.state)
---/c game.player.print(game.player.selected.train.path_end_stop)
---/c game.player.print(game.player.selected.direction)
---/c game.player.print(game.player.surface.find_entities_filtered{force = game.player.force, position = {x = 5, y = -9}, type = "locomotive"})
---/c game.player.print(game.player.selected.burner.currently_burning)
---/c game.player.print(game.player.selected.get_control_behavior().disabled
---/c game.player.print(game.player.selected.train.front_stock == game.player.selected)
---/c game.player.print(game.player.selected.circuit_connection_definitions[1].target_circuit_id)
